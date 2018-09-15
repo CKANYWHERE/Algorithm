574a8fc0ae1f407b19073bd9af7652e54d9742c9
M <janu723@naver.com>
2018-09-15 13:21:32 +0900

    토마토

--- Algorithm/Algorithm/7576.cpp
+++ Algorithm/Algorithm/7576.cpp
@@ -1,78 +1,56 @@
 #include "pch.h"
-#include <iostream>
-#include <algorithm>
+#include <cstdio>
 #include <queue>
 using namespace std;
-int arr[1001][1001];
-int dist[1001][1001];
-int dr[] = { 0,0,1,-1 };
-int dc[] = { 1,-1,0,0 };
-int n, m;
-int ans = 0;
-
-void bfs(int r, int c) {
-	
+int a[1000][1000];
+int d[1000][1000];
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+int main() {
+	int n, m;
+	scanf_s("%d %d", &m, &n);
 	queue<pair<int, int>> q;
-	q.push(make_pair(r, c));
-	
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf_s("%d", &a[i][j]);
+			d[i][j] = -1;
+			if (a[i][j] == 1) {
+				q.push(make_pair(i, j));
+				d[i][j] = 0;
+			}
+		}
+	}
 	while (!q.empty()) {
-		r = q.front().first;
-		c = q.front().second;
+		int x = q.front().first;
+		int y = q.front().second;
 		q.pop();
 		for (int k = 0; k < 4; k++) {
-			int nr = r + dr[k];
-			int nc = c + dc[k];
-			if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
-				if (arr[nr][nc] == 0 && dist[nr][nc] == -1) {
-					if (arr[nr][nc] == 1) {
-						return;
-					}
-					q.push(make_pair(nr, nc));
-					dist[nr][nc] = dist[r][c] + 1;
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+				if (a[nx][ny] == 0 && d[nx][ny] == -1) {
+					d[nx][ny] = d[x][y] + 1;
+					q.push(make_pair(nx, ny));
 				}
 			}
 		}
 	}
-}
-
-int main() {
-
-	cin >> n >> m;
-
+	int ans = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-			dist[i][j] = -1;
-			if (arr[i][j] == 1) {
-				dist[i][j] = 0;
-				bfs(i, j);
+			if (ans < d[i][j]) {
+				ans = d[i][j];
 			}
 		}
 	}
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cout << dist[i][j];
-		}
-		cout << "\n";
-	}
-
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j<m; j++) {
-			ans = max(ans, dist[i][j]);
-		}
-	}
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] == 0 && dist[i][j] == -1) {
+			if (a[i][j] == 0 && d[i][j] == -1) {
 				ans = -1;
 			}
 		}
 	}
 
-	cout << ans << "\n";
-
+	printf("%d\n", ans);
 	return 0;
 }
