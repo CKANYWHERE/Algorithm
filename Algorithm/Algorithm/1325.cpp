f5dd9d216bb821cab080a77739c3aa96b76b9faa
M <janu723@naver.com>
2018-09-24 21:17:09 +0900

    효율적인 해킹

--- Algorithm/Algorithm/1325.cpp
+++ Algorithm/Algorithm/1325.cpp
@@ -6,53 +6,52 @@
 using namespace std;
 
 vector<int> graph[10001];
+vector<int> nodes;
 bool check[10001];
+int maxValue = 0;
 int cnt = 0;
-int ans = 0;
-int n, m;
 
 void dfs(int node) {
-
-	if (check[node])
-		return;
-
-	check[node] = true;
-	cnt+=1;
-	ans = max(cnt,ans);
+	if (check[node]) return;
+	else check[node] = true;
+	cnt++;
 	for (int i = 0; i < graph[node].size(); i++) {
-		int next = graph[node][i];
-		if (!check[next]) {
-			check[next] = true;
-			dfs(next);
-		}	
+		int next_node = graph[node][i];
+		if(!check[next_node]) dfs(next_node);
 	}
 }
 
 int main() {
+	int n, m;
+	
 	cin >> n >> m;
+
 	for (int i = 0; i < m; i++) {
-		int a, b;
-		cin >> a >> b;
-		graph[b].push_back(a);
+		int start, end;
+		cin >> start >> end;
+		graph[end].push_back(start);
 	}
-	int maxNode=0;
-	vector<int>result;
+
+	vector<int> res;
+
 	for (int i = 1; i <= n; i++) {
+		memset(check, false, sizeof(check));
 		cnt = 0;
 		dfs(i);
-		if (maxNode == cnt) {
-			result.push_back(i);
+		maxValue = max(maxValue, cnt);
+		if (cnt == maxValue) {
+			res.push_back(i);
 		}
-		if (maxNode < cnt) {
-			maxNode = cnt;
-			result.clear();
-			result.push_back(i);
+		if (cnt > maxValue) {
+			res.clear();
+			res.push_back(i);
 		}
-		memset(check, false, sizeof(check));
 	}
-	for (int i = 0; i < result.size(); i++) {
-		cout << result[i] << " ";
+
+	for (int i = 0; i < res.size(); i++) {
+		cout << res[i] << " ";
 	}
 	cout << "\n";
+
 	return 0;
 }
