1541d4a2ffd5e458da5433b6cd58f14d890b9210
M <janu723@naver.com>
2018-12-04 14:13:46 +0900

    보물섬

--- Algorithm/Algorithm/2589.cpp
+++ Algorithm/Algorithm/2589.cpp
@@ -1,60 +1,68 @@
 #include "pch.h"
 #include <iostream>
+#include <string>
 #include <queue>
+#include <algorithm>
+#include <cstring> //memset
 using namespace std;
+const int MAX = 50;
 
-int dx[] = { -1, 0, 1, 0 };
-int dy[] = { 0, -1, 0, 1 };
-int max_value = 0;
-char arr[51][51];
-bool visited[51][51];
-int len[61][61];
+typedef struct
+{
+	int y, x;
+}Dir;
+Dir moveDir[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+int L, W;
+string graph[MAX];
+int cache[MAX][MAX];
 
-void bfs(int r, int c) {
+//전형적인 BFS
+
+int BFS(int y, int x)
+{
+	memset(cache, 0, sizeof(cache));
 	queue<pair<int, int>> q;
-	q.push(make_pair(r, c));
+	int result = 0;
+	q.push(make_pair(y, x));
+	cache[y][x] = 1;
+
 	while (!q.empty())
 	{
-		int root_r = q.front().first;
-		int root_c = q.front().second;
+		int curY = q.front().first;
+		int curX = q.front().second;
 		q.pop();
-		for (int i = 0; i < 4; i++) {
-			int nr = root_r + dx[i];
-			int nc = root_c + dy[i];
-			if (arr[nr][nc] == 'L' && visited[nr][nc] == 0) {
-				q.push(make_pair(nr, nc));
-				visited[nr][nc] = true;
-				len[nr][nc] = len[root_r][root_c] + 1;
-				if (max_value < len[nr][nc])
-					max_value = len[nr][nc];
-			}
+		for (int i = 0; i < 4; i++)
+		{
+			int nextY = curY + moveDir[i].y;
+			int nextX = curX + moveDir[i].x;
+			result = max(result, cache[curY][curX]);
+			//범위 내에 있고
+			if (0 <= nextY && nextY < L && 0 <= nextX && nextX < W)
+				//중복되지 않았고 육지이면 큐에 넣는다
+				if (graph[nextY][nextX] == 'L' && cache[nextY][nextX] == 0)
+				{
+					q.push(make_pair(nextY, nextX));
+					cache[nextY][nextX] = cache[curY][curX] + 1;
+				}
 		}
 	}
+	//result는 칸 수, 경로는 1을 빼야한다
+	return result - 1;
 }
 
-int main() {
+int main(void)
+{
+	cin >> L >> W;
+	for (int i = 0; i < L; i++)
+		cin >> graph[i];
 
-	int h, w; //y,x좌표
-	cin >> h >> w;
-
-
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			cin >> arr[i][j];
-			cout << arr[i][j];
-		}
-	}
-
-	for (int i = 0; i < h; i++) {
-		for (int j = 0; j < w; j++) {
-			if (arr[i][j] == 'L' && visited[i][j] == false) {
-				bfs(i, j);
-				memset(visited, 0, sizeof(visited));
-				memset(len, 0, sizeof(len));
-			}
-		}
-	}
-
-	cout << max_value << "\n";
+	int result = 0;
+	//brute force로 보물 위치 모두 찾아본다
+	for (int i = 0; i < L; i++)
+		for (int j = 0; j < W; j++)
+			if (graph[i][j] == 'L')
+				result = max(result, BFS(i, j));
+	cout << result << endl;
 	return 0;
+
 }
