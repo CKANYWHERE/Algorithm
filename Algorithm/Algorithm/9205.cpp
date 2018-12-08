ffad0c5495d17ba7d60c52a0a991b558dae0b777
M <janu723@naver.com>
2018-12-08 16:37:18 +0900

    맥주마시면서 걸어가기

--- Algorithm/Algorithm/9205.cpp
+++ Algorithm/Algorithm/9205.cpp
@@ -18,30 +18,28 @@ bool dist(int x1, int y1, int x2, int y2) {
 
 int main(void) {
 	cin >> t;
-	while (t--) {
+	for (int TEST = 0; TEST < t; TEST++) {
 		cin >> n;
-		check = vector<bool>(n + 2, false);
-		v = vector<pair<int, int> >(n + 2, make_pair(0, 0));
-		for (int i = 0; i < n + 2; i++)
-			cin >> v[i].first >> v[i].second;
-
+		check = vector<bool>(n + 2);
+		v = vector<pair<int, int>>(n + 2, make_pair(0, 0));
+		for (int i = 0; i < n + 2; i++) {
+			cin >> v[i].first, v[i].second;
+		}
 		queue<int> q;
 		q.push(0);
 		check[0] = true;
-
-		bool find = false;
+		
 		while (!q.empty()) {
 			int now = q.front();
 			q.pop();
-
 			for (int i = 1; i < n + 2; i++) {
 				if (!check[i] && dist(v[now].first, v[now].second, v[i].first, v[i].second)) {
 					q.push(i);
 					check[i] = true;
 				}
 			}
+		
 		}
-
 		if (check[n + 1] == true) {
 			cout << "happy" << "\n";
 		}
