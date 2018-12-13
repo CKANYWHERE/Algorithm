db0f9a65af3bfa1ee12284eaadce52fe79c5e672
M <janu723@naver.com>
2018-12-13 16:42:23 +0900

    효율적인 해킹

--- Algorithm/Algorithm/1325.cpp
+++ Algorithm/Algorithm/1325.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
 #include <cstring> 
 using namespace std;
 
@@ -11,43 +12,52 @@ bool check[10001];
 int maxValue = 0;
 int cnt = 0;
 
-void dfs(int node) {
-	if (check[node])
-		return;
-	check[node] = true;
-	cnt++;
-	for (int i = 0; i < graph[node].size(); i++) {
-		int next_node = graph[node][i];
-		if(!check[next_node]) dfs(next_node);
-	}
-}
-
 int main() {
 	int n, m;
 	cin >> n >> m;
+
 	for (int i = 0; i < m; i++) {
-		int start, end;
-		cin >> start >> end;
-		graph[end].push_back(start);
+		int a, b;
+		cin >> a >> b;
+		graph[b].push_back(a);
 	}
-
-	vector<int> ans;
-
+	vector<int> result;
 	for (int i = 1; i <= n; i++) {
-		memset(check, false, sizeof(check));
 		cnt = 0;
-		dfs(i);
-		maxValue = max(cnt, maxValue);
-		if (cnt == maxValue)
-			ans.push_back(i);
-		if (cnt > maxValue) {
-			ans.clear();
-			ans.push_back(i);
+		
+		queue<int> q;
+		q.push(i);
+		while (!q.empty()) {
+			cnt++;
+			int now = q.front();
+			check[now] = true;
+			q.pop();
+			for (int j = 0; j<graph[now].size(); j++) {
+				int next = graph[now][j];
+				if (!check[next]) {
+					check[next] = true;
+					q.push(next);
+				}
+			}
 		}
+		maxValue = max(cnt, maxValue);
+		if (cnt == maxValue) {
+			result.push_back(i);
+		}
+
+		if (cnt > maxValue) {
+			maxValue = cnt;
+			result.clear();
+			result.push_back(i);
+		}
+		memset(check, false, sizeof(check));
+		
 	}
 
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << " ";
+	sort(result.begin(), result.end());
+
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << " ";
 	}
 	cout << "\n";
 	return 0;
