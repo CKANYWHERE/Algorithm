553aef07e736998d3c0747071aeeb4aa6878ed02
Min-Chang-Kyeoung <janu723@naver.com>
2019-03-10 16:11:10 +0900

    카드 구매하기

--- Algorithm/Algorithm/11052.cpp
+++ Algorithm/Algorithm/11052.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
 int dp[1001];
 int p[100001];
 int main() {
-
 	int n;
 	cin >> n;
 
@@ -14,16 +12,14 @@ int main() {
 		cin >> p[i];
 	}
 
-	dp[0] = 0;
+	dp[1] = p[1];
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
-			dp[i] = max(dp[i],p[j] + dp[i-j]);
+			dp[i] = max(dp[i], p[j] + dp[i - j]);
 		}
 	}
 
 	cout << dp[n] << "\n";
-
-
 	return 0;
 }
